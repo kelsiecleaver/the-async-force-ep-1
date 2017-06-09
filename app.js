9c04667003d343aaafe238a681cb83d9c0cffb2d
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-06-08 17:32:31 -1000

    han solo finished

--- app.js
+++ app.js
@@ -17,11 +17,37 @@
     person4HomeWorld.appendChild(person4Home);
 
   }
+  function reqListener2(){
+    var response = JSON.parse(this.responseText);
+    var person14 = document.getElementById("person14Name");
+    person14.innerHTML = response.name;
+    person14name.appendChild(person14);
+  }
+
+  function reqListener3(){
+    var response = JSON.parse(this.responseText);
+    var person14Species = document.getElementById("person14Species");
+    person14Species.innerHTML = response.name;
+    person14Species.appendChild(person14Species);
+  }
+  function filmListener(){
+    var response = JSON.parse(this.responseText);
+    var filmList = document.getElementById("filmList");
+    filmList.innerHTML = response.name;
+    filmList.appendChild(filmList);
+  }
 
   function getPerson(){
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
   oReq.open("GET", "http://swapi.co/api/people/4/");
+  oReq.send();
+}
+
+function getPerson14(){
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener2);
+  oReq.open("GET", "http://swapi.co/api/people/14/");
   oReq.send();
 }
   function getHome(){
@@ -30,7 +56,26 @@
     oReq.open("GET", "http://www.swapi.co/api/planets/1/");
     oReq.send();
   }
+  function getHome14(){
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener3);
+    oReq.open("GET", "http://swapi.co/api/species/1/");
+    oReq.send();
+  }
+  function getFilm(){
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", filmListener);
+    oReq.open("GET", " http://swapi.co/api/films/");
+    oReq.send();
+  }
+
+
+
+
+
 getPerson();
 getHome();
-
+getPerson14();
+getHome14();
+getFilm();
 })();
